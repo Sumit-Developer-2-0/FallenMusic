9c950116ed737e97c737b26ac693c51b605e9362
Sumit-Developer-2-0 <sumitdeveloper2.0@gmail.com>
2025-02-09 14:44:19 +0530

    Update clear.py
    Signed-off-by: Sumit-Developer-2-0 <sumitdeveloper2.0@gmail.com>

--- FallenMusic/Helpers/clear.py
+++ FallenMusic/Helpers/clear.py
@@ -1,32 +1,26 @@
-# MIT License
-#
-# Copyright (c) 2023 AnonymousX1025
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+import logging
 
 from FallenMusic import fallendb
 from FallenMusic.Helpers import remove_active_chat
 
+# Configure logging (if not already configured)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 async def _clear_(chat_id):
     try:
-        fallendb[chat_id] = []
+        # Assuming fallendb is a dictionary-like object for demonstration
+        if chat_id in fallendb:
+            fallendb[chat_id] = []
+        else:
+            logging.warning(f"Chat ID {chat_id} not found in fallendb.")
+            return  # Or create the entry if appropriate: fallendb[chat_id] = []
+
         await remove_active_chat(chat_id)
-    except:
-        return
+        logging.info(f"Cleared music queue and removed active chat for chat ID {chat_id}")
+
+    except KeyError:
+        logging.error(f"KeyError: Chat ID {chat_id} not found in fallendb.")
+    except Exception as e:
+        logging.exception(f"An unexpected error occurred while clearing chat {chat_id}: {e}")
+        # Consider re-raising the exception if you can't handle it completely
+        # raise
